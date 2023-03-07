45b036aa1b6faa520a90c32b4a1a63cf5e5e35a9
Xei <jessen.marcus@gmail.com>
2023-03-07 15:41:17 +0100

    Plotted B sensors

--- NolekMLApp/classes.py
+++ NolekMLApp/classes.py
@@ -7,44 +7,51 @@ class testData:
     b32 = []
     b22 = []
     
-    x_max = dt.datetime()
-    y_max = float()
+    # x_max = dt.datetime()
+    # y_max = float()
     
     def __init__(self, _logTime, _stepNo, _b31, _b32, _b22):
+        # self.SetLogTime(_logTime)
+        self.SetStepNo(_stepNo)
+        self.SetB31(_b31)
+        self.SetB32(_b32)
+        self.SetB22(_b22)
         
+    def SetLogTime(self, valList):
+        for x in valList:
+            self.logTime.append(dt.datetime(x))
     
-    def SetTestDuration(self, x):
-        self.test_Dur = x[0]
-        
-    def SetXValues(self, valList):
-        self.x_vals = range(0, len(valList))
+    def SetStepNo(self, valList):
+        for x in valList:
+            self.stepNo.append(x)
     
     def SetB31(self, valList):
-        y_list = []
-        for x in valList:
-            y_list = list(x.split(";"))
-        
-        for i in y_list:
-            self.y_vals.append(float(i))
+        for str in valList:
+            if "," in str:
+                myStr = str.replace(",", ".")
+                self.b31.append(float(myStr))
+            else:
+                self.b31.append(float(str))
     
     def SetB32(self, valList):
-        y_list = []
-        for x in valList:
-            y_list = list(x.split(";"))
-        
-        for i in y_list:
-            self.y_vals.append(float(i))
+        for str in valList:
+            if "," in str:
+                myStr = str.replace(",", ".")
+                self.b32.append(float(myStr))
+            else:
+                self.b32.append(float(str))
 
-    def SetB22(self, valList):
-        y_list = []
-        for x in valList:
-            y_list = list(x.split(";"))
+    def SetB22(self, valList): 
+        for str in valList:
+            if "," in str:
+                myStr = str.replace(",", ".")
+                self.b22.append(float(myStr))
+            else:
+                self.b22.append(float(str))
+
     
-        for i in y_list:
-            self.y_vals.append(float(i))
-    
-    def FindYMax(self):
-        for x in self.y_vals:
-            if x > self.y_max:
-                self.y_max = x
-        self.y_max = self.y_max + (self.y_max/100*10)
+    # def FindYMax(self):
+    #     for x in self.y_vals:
+    #         if x > self.y_max:
+    #             self.y_max = x
+    #     self.y_max = self.y_max + (self.y_max/100*10)
--- NolekMLApp/program.py
+++ NolekMLApp/program.py
@@ -1,21 +1,34 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+import classes as cls
 
-
-plt.style.use("fivethirtyeight")
-
-data = pd.read_csv('C:\\Users\Marcus\Documents\studious-chainsaw\Marcus-Test-Data\D22102_R1.1_NolekTest_230226.csv')
+data = pd.read_csv('C:\\Users\Marcus\Documents\studious-chainsaw\Marcus-Test-Data\D22102_R1.1_NolekTest_230226.csv', sep=';')
 logTime = data['LogTime']
 stepNo = data['StepNo']
-circuitName = data['CircuitName']
-tmp1 = data['TMP1']
-tmp2 = data['TMP2']
 b31 = data['B31']
 b32 = data['B32']
-b21 = data['B21']
 b22 = data['B22']
-p101 = data['P101']
-regSP = data['RegulatorSP']
-regFB = data['RegulatorFB']
 
-print(b22)
+test = cls.testData(logTime, stepNo, b31, b32, b22)
+
+# print(type(test.logTime[0]))
+print(type(test.stepNo[0]))
+print(type(test.b31[0]))
+print(type(test.b32[0]))
+print(type(test.b22[0]))
+
+fig, ax1 = plt.subplots()
+
+ax2 = ax1.twinx()
+ax1.plot(test.b22, 'b', label='Presure at B22 sensor')
+ax2.plot(test.b31, 'g--', label='Temperature at B31 sensor')
+ax2.plot(test.b32, 'r--', label='Temperature at B32 sensor')
+
+ax1.set_ylabel("Presure in bar")
+ax2.set_ylabel("Temperature in celcius")
+
+plt.title("Presure Test")
+plt.tight_layout()
+ax1.legend(loc=9)
+ax2.legend()
+plt.show()
